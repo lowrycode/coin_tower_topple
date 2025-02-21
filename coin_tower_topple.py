bfd0fad52d87930aeee0750eb2bc6aa4074b9f90
Pete Lowry <peter.m.lowry@gmail.com>
2025-02-21 17:30:54 +0000

    Add _get_settings_str method for displaying game settings when app is launched

--- coin_tower_topple.py
+++ coin_tower_topple.py
@@ -7,8 +7,31 @@ class CoinTowerTopple:
     Use the start() method to launch the app
     """
 
+    DIFFICULTY_DESCRIPTION_MAP = {1: "Easy", 2: "Medium", 3: "Hard"}
+
     def __init__(self):
-        print("Initialising CoinTowerTopple")
+        # Game settings
+        self.difficulty_level = 1
+        self.topple_height = 21
+        self.possible_actions = [1, 2, 3]
 
     def start(self):
-        print("Launching app")
+        """
+        Launches the app.
+
+        Displays the game title and default game settings before starting the
+        Main Menu loop.
+        """
+        print("\n\n#### COIN TOWER TOPPLE ####\n")
+        print(self._get_settings_str())
+
+    def _get_settings_str(self):
+        settings_str = (
+            "GAME SETTINGS\n"
+            f"{'- Difficulty: ':<20} "
+            f"{self.DIFFICULTY_DESCRIPTION_MAP[self.difficulty_level]}\n"
+            f"{'- Topple Height: ':<20} {self.topple_height}\n"
+            f"{'- Possible Actions: ':<20} "
+            f"{', '.join(map(str, self.possible_actions))}"
+        )
+        return settings_str
