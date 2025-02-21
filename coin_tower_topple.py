abdd72313b00adf9c42e06cfb7c7346fb1525aac
Pete Lowry <peter.m.lowry@gmail.com>
2025-02-21 17:04:17 +0000

    Add coin_tower_topple.py including CoinTowerTopple class and connect to run.py

---
+++ coin_tower_topple.py
@@ -0,0 +1,14 @@
+class CoinTowerTopple:
+    """
+    Implements the logic for the Coin Tower Topple game, including:
+    - menu navigation and user input validation
+    - main game-play logic
+
+    Use the start() method to launch the app
+    """
+
+    def __init__(self):
+        print("Initialising CoinTowerTopple")
+
+    def start(self):
+        print("Launching app")
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from coin_tower_topple import CoinTowerTopple
+
+
+def main():
+    app = CoinTowerTopple()
+    app.start()
+
+
+main()
