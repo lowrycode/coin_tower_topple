d64c2bdf36e6a635f2fe0d24921d5e6f805e4de7
Pete Lowry <peter.m.lowry@gmail.com>
2025-02-27 22:02:15 +0000

    Add docstring to main function

--- run.py
+++ run.py
@@ -2,6 +2,12 @@ from coin_tower_topple import CoinTowerTopple
 
 
 def main():
+    """
+    Starts the CoinTowerTopple game.
+
+    This function creates an instance of the CoinTowerTopple class and
+    starts the game by calling its start() method.
+    """
     app = CoinTowerTopple()
     app.start()
 
